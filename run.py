c56239fb1507fe456c546e0aba2924e174d1804d
d 3 n 7 <29033313+d3n7@users.noreply.github.com>
2023-05-01 15:06:11 +0200

    Update run.py

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from langchain.agents import Tool
 from langchain.agents import load_tools
 from langchain.agents import initialize_agent
 from langchain.agents import AgentType
-from langchain.tools import DuckDuckGoSearchTool
+from langchain.tools import DuckDuckGoSearchRun
 from langchain.callbacks import get_openai_callback
 from langchain.schema import (
     AIMessage,
@@ -14,36 +14,39 @@ from langchain.schema import (
     SystemMessage
 )
 
-#authentication
+# authentication
 with open('auth.txt', 'r') as f:
     auth = f.readlines()[0].strip()
 os.environ['OPENAI_API_KEY'] = auth
 cost = 0
 
-#set up GPT-3.5 Chat Bot
+# set up GPT-3.5 Chat Bot
 chat = ChatOpenAI(temperature=0)
 history = []
 
-#set up GPT-3.5 agent tools
+# set up GPT-3.5 agent tools
 llm = OpenAI()
-agentTools = [DuckDuckGoSearchTool()]
+agentTools = [DuckDuckGoSearchRun()]
 agentTools.extend(load_tools(['llm-math', 'terminal'], llm=llm))
 
-#set up GPT-3.5 director
+# set up GPT-3.5 director
 dllm = ChatOpenAI(temperature=0)
 directorSys = 'Does the following request require searching the internet, interacting with the filesystem, ' \
-                 'executing code, or doing math calculations?\nRespond only with yes or no.'
+              'executing code, or doing math calculations?\nRespond only with yes or no.'
+
+
 def direct(query):
+    query = '"{}"'.format(query)
     yn = dllm([SystemMessage(content=directorSys), HumanMessage(content=query)]).content.lower()
     return 'yes' in yn
 
-#Main loop
+
+# Main loop
 print('Type exit to quit')
 while 1:
     prompt = input('> ')
     if prompt.lower() == 'exit':
-        print('Total cost: $'+str(cost))
-        print('currently missing non-agent costs')
+        print('Total cost: $' + str(cost))
         exit()
 
     with get_openai_callback() as cb:
@@ -51,14 +54,15 @@ while 1:
         answer = ''
         if direct(prompt):
             memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-            #give the agent 1 message of context if there is any
+            # give the agent 1 message of context if there is any
             if len(history) > 1:
                 memory.chat_memory.add_ai_message(history[-2].content)
-            agent = initialize_agent(tools=agentTools, llm=chat, agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION, verbose=True, memory=memory)
+            agent = initialize_agent(tools=agentTools, llm=chat, agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION,
+                                     verbose=True, memory=memory)
             try:
                 answer = agent.run(prompt)
             except Exception as e:
-                print('ERROR: '+str(e))
+                print('ERROR: ' + str(e))
         else:
             answer = chat(history).content
         history.append(AIMessage(content=answer))
